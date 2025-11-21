a12eff40ebf0199ad36808ec8a4291be1a017688
Vlad Garbuz <vlgar25@student.sdu.dk>
2025-11-21 11:24:56 +0100

    Add test.txt to Presentation project
    Added a new empty file test.txt in the src/Presentation directory. Updated assembly info and cache files as a result of the build process.

--- src/obj/Debug/net9.0/OopWorkshop.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/OopWorkshop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OopWorkshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45f62250514c8bb679438f5bec0072c626f7b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b0ab5d10c011c7d738127332b03bcffba2fb7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OopWorkshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OopWorkshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
